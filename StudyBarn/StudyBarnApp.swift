f55daca587e478046e45a6ce051db12765024586
anng-y <anngyip59@gmail.com>
2024-02-15 16:40:59 -0800

    Initial Commit

---
+++ StudyBarn/StudyBarnApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBarnApp.swift
+//  StudyBarn
+//
+//  Created by Ann Yip on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyBarnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
