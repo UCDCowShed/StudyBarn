e48663b94af9fdf010beb13cf4e79a304efdcf22
JinLee <yjzlee@ucdavis.edu>
2024-02-27 18:11:48 -0800

    complete userModel

--- StudyBarn/Login/LoginViewModel.swift
+++ StudyBarn/Login/LoginViewModel.swift
@@ -13,6 +13,21 @@ final class LoginViewModel: ObservableObject {
     func signInGoogle() async throws {
         let helper = SignInGoogleHelper()
         let tokens = try await helper.signIn()
-        try await AuthenticationManager.shared.signInWithGoogle(tokens: tokens)
+        
+        // Get User after authentication from Google
+        let authResult = try await AuthenticationManager.shared.signInWithGoogle(tokens: tokens)
+        
+        // Initialize a USER with auth from Google
+        let user = UserModel(auth: authResult)
+        
+        // Check if user exists
+        let userExists = try await UserManager.shared.getUser(userId: user.userId)
+        
+        // User Does not exist
+        if userExists == nil {
+            // Create User into the database
+            try await UserManager.shared.createNewUser(user: user)
+            print("User Created")
+        }
     }
 }
