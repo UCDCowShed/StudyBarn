bd9df4df183b50313e2864f02633143df7297c91
JinLee <yjzlee@ucdavis.edu>
2024-02-29 23:34:29 -0800

    added get all location feature

--- StudyBarn/Location/LocationBoxView.swift
+++ StudyBarn/Location/LocationBoxView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct LocationBoxView: View {
     @State var heart = "heart"
+    let area: AreaModel?
     
-    var images = [
+    public init(area: AreaModel?) {
+            self.area = area
+    }
+    
+    private var images = [
         "Shields-outside",
         "Shields-inside",
         "Shields-reading",
         "Shields-tutor"
     ]
+    
+    private func formatHours (hours: HourMin?) -> String {
+        guard let hours = hours else { return "00:00"}
+        
+        // PM
+        if hours.hour > 12 {
+            let formatHour = hours.hour - 12
+            return "\(formatHour):\(hours.minute) PM"
+        }
+        // 12 PM
+        else if hours.hour == 12 {
+            return "\(hours.hour):\(hours.minute) PM"
+        }
+        // AM
+        else {
+            return "\(hours.hour):\(hours.minute) AM"
+        }
+        
+    }
     
     var body: some View {
         VStack(spacing: 8) {
@@ -31,16 +55,16 @@ struct LocationBoxView: View {
             .tabViewStyle(.page)
             HStack(alignment: .top) {
                 VStack(alignment: .leading) {
-                    Text("Peter S. Shields Library")
+                    Text(area?.name ?? "Area")
                         .font(.title)
-                    Text("7:30am - 12:00am")
+                    Text("\(formatHours(hours: area?.openHour)) - \(formatHours(hours: area?.closeHour))")
                         .font(.subheadline)
                 }
                 Spacer()
                 VStack {
                     HStack(spacing: 2) {
                         Image(systemName: "star.fill")
-                        Text("5.00")
+                        Text("\(area?.rating ?? 5, specifier: "%.1f")")
                     }
                     .font(.subheadline)
                     Button {
@@ -51,14 +75,13 @@ struct LocationBoxView: View {
                             .foregroundColor(Color.red.opacity(0.8))
                     }
                 }
+                
             }
             .padding()
         }
-
-
     }
 }
 
 #Preview {
-    LocationBoxView()
+    LocationBoxView(area: nil)
 }
