799b382808d75c85fcad542c95045a5886644252
Jin Ho Yon <jyon@ucdavis.edu>
2024-03-07 23:45:29 -0800

    Update HeartButtonView.Swift

--- StudyBarn/Components/HeartButtonView.swift
+++ StudyBarn/Components/HeartButtonView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 @MainActor
 final class HeartButtonViewModel: ObservableObject {
-    func toggleFavorite(heart: String) -> String {
-        // add functionality for adding and removing the favorite from the user
+    func toggleFavorite(heart: String, userId: String, favorite: String) -> String {
+        if heart == "heart" {
+                    // add functionality for adding and removing the favorite from the user
+                    do {
+                        try await addUserFavorite(userId: userId, favorite: favorite)
+                    } catch {
+                        // error
+                        print("Error adding favorite: \(error)")
+                    }
+                } else {
+                    removeUserFavorite(userId: userId, favorite: favorite)
+                }
+        
         return heart == "heart" ? "heart.fill" : "heart"
     }
 }
